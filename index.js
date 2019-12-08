58ec938799ec84fa554f7416fc6f09b8c53b263e
ibraheem Hasan <rules656@gmail.com>
2019-12-08 16:29:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Add your code here
+function submitData (name, email){
+  const url = 'http://localhost:3000/users'
+  const requestOptions = {
+    headers: {
+            Accept: "application/json"
+      "Content-Type": "application/json"
+    },
+    method: "POST",
+    body: JSON.stringify({name: name, email:email})
+  }
+  return fetch(url,requestOptions)
+}
+submitData("John", "john@invisible.com").then(res => {return res.json()}).then(json => {console.log(json)})
